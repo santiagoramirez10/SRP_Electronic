3f8a7e8be7aa3094e5dc9e11fcabc660f56f9a58
santiagoramirez10 <santiagoramirezperez96@gmail.com>
2023-09-08 13:00:59 -0500

    Json files

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,10 +1,13 @@
 import { RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { ProductsComponent } from './products/products.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { Component } from '@angular/core';
 
 const appRoutes = [
     { path: "", component: HomeComponent },
     { path: "home", component: HomeComponent },
+    { path: "products", component: ProductsComponent },
     { path: '**', component: PageNotFoundComponent }
 ];
 export const routing = RouterModule.forRoot(appRoutes);
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,13 @@
 import { Component } from '@angular/core';
+import ProductsJson from "./products.json";
+
+interface PRODUCTS {
+  Codigo: String,
+  Nombre_Producto: String;
+  Precio: String;
+  Inventario: String;
+  Imagen: String;
+}
 
 @Component({
   selector: 'app-home',
@@ -6,5 +15,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
+  Products: PRODUCTS[] = ProductsJson;
+  constructor() {
+    console.log(this.Products);
+  }
+
 
 }
